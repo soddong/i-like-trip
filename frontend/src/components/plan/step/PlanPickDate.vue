4179cd19cd9d657ad0b2c968664c275421925ad9
styughjvbn <3338761@naver.com>
2024-05-17 20:08:11 +0900

    [front] refactor : 선택한 일정 기간 확인 컴포넌트 분리
    선택한 일정 기간을 간략하게 볼 수 있는 컴포넌트로 분리

--- frontend/src/components/plan/step/PlanPickDate.vue
+++ frontend/src/components/plan/step/PlanPickDate.vue
@@ -1,26 +1,16 @@
 <script setup>
 import { usePlanStore } from "@/stores/plan"
 import { storeToRefs } from "pinia"
+import PlanCheckDate from "./item/PlanCheckDate.vue";
 
 const planStore = usePlanStore()
-const { getStartEnd } = planStore
 const { period } = storeToRefs(planStore)
 
 </script>
 
 <template>
-    <v-container min-width="400">
-        <v-row justify="center">
-            <v-col>
-                출발 <br>
-                {{ getStartEnd().start }}
-            </v-col>
-            ~
-            <v-col>
-                도착 <br>
-                {{ getStartEnd().end }}
-            </v-col>
-        </v-row>
+    <v-container fluid>
+        <PlanCheckDate />
         <v-row justify="center">
             <v-date-picker show-adjacent-months multiple="range" v-model="period" />
         </v-row>
---
+++ frontend/src/components/plan/step/item/PlanCheckDate.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { usePlanStore } from "@/stores/plan"
+import { mdiPlay } from '@mdi/js';
+
+const planStore = usePlanStore()
+const { getStartEnd } = planStore
+</script>
+
+<template>
+    <v-row justify="center">
+        <v-col class="text-center ">
+            <p style="font-size: x-large;">출발</p>
+            <p style="font-size: small;">
+                {{ getStartEnd().start }}
+            </p>
+        </v-col>
+        <div class="d-flex align-center" >
+            <v-icon :icon="mdiPlay"></v-icon>
+        </div>
+        
+        <v-col class="text-center">
+            <p style="font-size: x-large;">도착</p>
+            <p style="font-size: small;">
+                {{ getStartEnd().end }}
+            </p>
+        </v-col>
+    </v-row>
+</template>
+
+<style scoped></style>
