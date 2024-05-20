9b34bc53b75953ff5249329969f0ef95e7b78bca
styughjvbn <3338761@naver.com>
2024-05-21 02:09:23 +0900

    [back] feat : attraction 검색시 attractionType 다중값 허용

--- backend/src/main/java/com/ssafy/attraction/model/AttractionSearchDto.java
+++ backend/src/main/java/com/ssafy/attraction/model/AttractionSearchDto.java
@@ -1,58 +1,19 @@
 package com.ssafy.attraction.model;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class AttractionSearchDto {
-	int sidoCode;
-	int gugunCode;
-	int contentType;
+	Integer sidoCode;
+	Integer gugunCode;
+	List<Integer> contentType;
 	String keyword;
-
-	public AttractionSearchDto() {
-	}
-
-	public AttractionSearchDto(int sidoCode, int gugunCode, int contentType, String keyword) {
-		super();
-		this.sidoCode = sidoCode;
-		this.gugunCode = gugunCode;
-		this.contentType = contentType;
-		this.keyword = keyword;
-	}
-
-	public int getSidoCode() {
-		return sidoCode;
-	}
-
-	public void setSidoCode(int sidoCode) {
-		this.sidoCode = sidoCode;
-	}
-
-	public int getGugunCode() {
-		return gugunCode;
-	}
-
-	public void setGugunCode(int gugunCode) {
-		this.gugunCode = gugunCode;
-	}
-
-	public int getContentType() {
-		return contentType;
-	}
-
-	public void setContentType(int contentType) {
-		this.contentType = contentType;
-	}
-
-	public String getKeyword() {
-		return keyword;
-	}
-
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
-	}
-
-	@Override
-	public String toString() {
-		return "SearchTripDto [sidoCode=" + sidoCode + ", gugunCode=" + gugunCode + ", contentType=" + contentType
-				+ ", keyword=" + keyword + "]";
-	}
-
 }
