f7ec99e082362b2ad5cff6f17e46fab40a407908
서종원 <s3338761@naver.com>
2024-05-21 13:28:05 +0900

    [back] fix : 여행 일정 DTO에 타이틀 추가

--- backend/src/main/java/com/ssafy/plan/model/PlaceDto.java
+++ backend/src/main/java/com/ssafy/plan/model/PlaceDto.java
@@ -1,58 +1,23 @@
 package com.ssafy.plan.model;
 
-import com.ssafy.attraction.model.AttractionDto;
+import java.util.List;
 
+import com.ssafy.attraction.model.AttractionDto;
+import com.ssafy.member.model.MemberDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class PlaceDto {
 	private int order;
 	private String startTime;
 	private String endTime;
 	private String comment;
 	private AttractionDto place;
-
-	public int getOrder() {
-		return order;
-	}
-
-	public void setOrder(int order) {
-		this.order = order;
-	}
-
-	public String getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(String startTime) {
-		this.startTime = startTime;
-	}
-
-	public String getEndTime() {
-		return endTime;
-	}
-
-	public void setEndTime(String endTime) {
-		this.endTime = endTime;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	public AttractionDto getPlace() {
-		return place;
-	}
-
-	public void setPlace(AttractionDto place) {
-		this.place = place;
-	}
-
-	@Override
-	public String toString() {
-		return "PlaceDto [order=" + order + ", startTime=" + startTime + ", endTime=" + endTime + ", comment=" + comment
-				+ ", place=" + place + "]";
-	}
-
 }
