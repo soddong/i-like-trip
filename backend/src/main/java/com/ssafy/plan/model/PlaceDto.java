e909f5e4ba6d95449bfca9853ee5c3123892798e
최소현 <aledjdygus9300@gmail.com>
2024-05-22 09:48:19 +0900

    [back] fix: 데이터 형식 매치되지 않는 문제 해결

--- backend/src/main/java/com/ssafy/plan/model/PlaceDto.java
+++ backend/src/main/java/com/ssafy/plan/model/PlaceDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.plan.model;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.ssafy.attraction.model.AttractionDto;
@@ -16,8 +17,8 @@ import lombok.ToString;
 @ToString
 public class PlaceDto {
 	private int order;
-	private String startTime;
-	private String endTime;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
 	private String comment;
 	private AttractionDto place;
 }
