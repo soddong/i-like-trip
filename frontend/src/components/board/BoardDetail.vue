eaaaff8b6ed325c883c1712e20f3ff9870f0715c
서종원 <s3338761@naver.com>
2024-05-23 12:24:35 +0900

    [common] securiy : 게시판 권한 별 기능 관리

--- frontend/src/components/board/BoardDetail.vue
+++ frontend/src/components/board/BoardDetail.vue
@@ -2,21 +2,23 @@
 import { ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { detailArticle, deleteArticle } from "@/api/board";
+import { useUserStore } from "@/stores/user";
 
-import BoardViewerItem from '@/components/board/item/BoardViewerItem.vue'; 
+import BoardViewerItem from '@/components/board/item/BoardViewerItem.vue';
 
-import '@/assets/styles.css'; 
+import '@/assets/styles.css';
 import defaultProfile from '@/assets/default_profile.png';
 
 const VITE_VUE_API_URL = import.meta.env.VITE_VUE_API_URL;
 
 const route = useRoute();
 const router = useRouter();
-
+const userStore = useUserStore();
 const articleUserProfile = ref(defaultProfile);
 const { articleno } = route.params;
 
 const article = ref({});
+const isMine = ref(false)
 
 onMounted(() => {
   getArticle();
@@ -32,6 +34,9 @@ const getArticle = () => {
         articleUserProfile.value = `${VITE_VUE_API_URL}upload?name=${article.value.profile}`;
       } else {
         articleUserProfile.value = defaultProfile;
+      }
+      if (article.value.userId === userStore.userId) {
+        isMine.value = true
       }
       console.log("Article Data:", article.value);
       console.log("Updated userProfile URL:", articleUserProfile.value);
@@ -53,17 +58,17 @@ function moveModify() {
 function onDeleteArticle() {
   if (confirm('게시글을 정말 삭제하시겠습니까?')) {
     deleteArticle(
-    articleno,
-    (response) => {
-      if (response.status == 200) {
-        moveList();
-        alert('게시글이 삭제되었습니다')
+      articleno, article.value.userId,
+      (response) => {
+        if (response.status == 200) {
+          moveList();
+          alert('게시글이 삭제되었습니다')
+        }
+      },
+      (error) => {
+        console.log(error);
       }
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+    );
   }
 }
 </script>
@@ -73,29 +78,29 @@ function onDeleteArticle() {
   <v-container>
     <v-row justify="center">
       <v-col cols="7">
-          <div class="article-header">
-            <h2 class="article-title">{{ article.subject }}</h2>
-          </div>
-          <div class="article-details">
-            <v-avatar color="brown">
-              <img :src="articleUserProfile" alt="Profile Image" class="cover-image">
-            </v-avatar>
-            <span class="user-name">{{ article.userName }}</span>
-            <span class="register-time">{{ article.registerTime }}</span>
-            <span class="hit">조회수 {{ article.hit }}회</span>
-          </div>
-          <v-divider></v-divider>
-          <div class="article-content">
-            <BoardViewerItem :content="article.content" />
-          </div>
-          <div class="article-actions">
-            <v-col cols="6">
-              <v-btn @click="onDeleteArticle" variant="tonal" class="mr-2">글삭제</v-btn>
-            </v-col>
-            <v-col cols="6">
-              <v-btn @click="moveModify" color="primary" variant="tonal" class="mr-2">글수정</v-btn>
-            </v-col>
-          </div>
+        <div class="article-header">
+          <h2 class="article-title">{{ article.subject }}</h2>
+        </div>
+        <div class="article-details">
+          <v-avatar color="brown">
+            <img :src="articleUserProfile" alt="Profile Image" class="cover-image">
+          </v-avatar>
+          <span class="user-name">{{ article.userName }}</span>
+          <span class="register-time">{{ article.registerTime }}</span>
+          <span class="hit">조회수 {{ article.hit }}회</span>
+        </div>
+        <v-divider></v-divider>
+        <div class="article-content">
+          <BoardViewerItem :content="article.content" />
+        </div>
+        <div class="article-actions" v-if="isMine">
+          <v-col cols="6">
+            <v-btn @click="onDeleteArticle" variant="tonal" class="mr-2">글삭제</v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn @click="moveModify" color="primary" variant="tonal" class="mr-2">글수정</v-btn>
+          </v-col>
+        </div>
 
       </v-col>
     </v-row>
@@ -140,7 +145,9 @@ function onDeleteArticle() {
   justify-content: flex-end;
 }
 
-.user-name, .register-time, .hit {
+.user-name,
+.register-time,
+.hit {
   margin-left: 20px;
   font-size: 16px;
   color: #666;
